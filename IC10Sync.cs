63344670120237f591a396286c8392bc1a1143bf
simon <simon.ogordnik@gmail.com>
2025-10-06 23:50:46 +0200

    Disallow direct editing of IC10s, add editing of laptop/computer sources

--- IC10Sync.cs
+++ IC10Sync.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using Assets.Scripts;
 using Assets.Scripts.Objects.Electrical;
+using Assets.Scripts.Objects.Motherboards;
 using Assets.Scripts.Util;
 using HarmonyLib;
 using StationeersMods.Interface;
 
 namespace IC10Sync
 {
-    [StationeersMod("IC10Sync", "IC10Sync", "0.2.5499.24517.1")]
+    [StationeersMod("IC10Sync", "IC10Sync", "0.2.5499.24517.2")]
     public class IC10Sync : ModBehaviour
     {
         // Config entry for chip data path
         private static string _chipDataPathResolved;
+
+        private static bool _allowDirectChipEditingInSurvival = false;
 
         public override void OnLoaded(ContentHandler contentHandler)
         {
@@ -20,6 +23,9 @@ namespace IC10Sync
 
             // Config for file path
             var chipDataPath = Config.Bind("Output", "ChipDataPath", @"%USERPROFILE%\Documents\My games\Stationeers\chipdata", "Path to write chip data").Value;
+
+            // Config for allowing direct chip editing in survival mode
+            _allowDirectChipEditingInSurvival = Config.Bind("Settings", "AllowDirectChipEditingInSurvival", false, "Allow direct editing of IC10 chips in survival mode (Otherwise, allow to only edit source code in computer/laptop)").Value;
 
             var harmony = new Harmony("IC10Sync");
             harmony.PatchAll();
@@ -57,8 +63,7 @@ namespace IC10Sync
         {
             try
             {
-                // Find all ProgrammableChip instances in the scene
-                var chips = FindObjectsOfType<ProgrammableChip>();
+
                 string directory = _chipDataPathResolved;
 
                 if (!System.IO.Directory.Exists(directory))
@@ -66,47 +71,80 @@ namespace IC10Sync
                     System.IO.Directory.CreateDirectory(directory);
                 }
 
-                foreach (var chip in chips)
+                // In survival mode, only update chips if allowed by config
+                if (WorldManager.Instance.GameMode == GameMode.Creative || _allowDirectChipEditingInSurvival)
                 {
-                    string chipName = chip.DisplayName ?? "UNNAMED";
-                    var key = $"{chipName} [{chip.ReferenceId}]";
-                    string fileName = $"{key}.ic10";
-                    string filePath = System.IO.Path.Combine(directory, fileName);
+                    // Find all ProgrammableChip instances in the scene
+                    var chips = FindObjectsOfType<ProgrammableChip>();
 
-                    var chipSourceCode = chip.GetSourceCode().ToString();
-                    var hashBefore = chipSourceCode.GetHashCode();
-                    
-
-                    if (!System.IO.File.Exists(filePath) || !codeHashes.ContainsKey(key) || codeHashes[key] != hashBefore) // file doesn't exist or code has changed
+                    foreach (var chip in chips)
                     {
-                        UnityEngine.Debug.LogWarning($"Chip {key} source code exported. Before: {codeHashes.GetValueOrDefault(key)}, After: {hashBefore}");
-                        ConsoleWindow.Print($"Chip {key} source code exported. Before: {codeHashes.GetValueOrDefault(key)}, After: {hashBefore}", ConsoleColor.Blue);
-                        System.IO.File.WriteAllText(filePath, chipSourceCode);
+                        var filePrefix = $"{chip.DisplayName ?? "UNNAMED"} [{chip.ReferenceId}]";
+                        if (UpdateChipDataIfNecessary(filePrefix, chip, directory))
+                        {
+                            return true; // If any chip was updated, return true to use shorter delay
+                        }
                     }
-                    codeHashes[key] = hashBefore; // store current hash
-
-                    // Check if the file exists and read its content
-                    var onDiskSourceCode = System.IO.File.Exists(filePath) ? System.IO.File.ReadAllText(filePath) : string.Empty;
-                    var onDiskHash = onDiskSourceCode.GetHashCode();
-
-                    if (onDiskHash != hashBefore)
-                    {
-                        UnityEngine.Debug.LogWarning($"Chip {key} source code loaded. Before: {hashBefore}, After: {onDiskHash}");
-                        ConsoleWindow.Print($"Chip {key} source code loaded. Before: {hashBefore}, After: {onDiskHash}", ConsoleColor.Green);
-                        // If the hash doesn't match, write the new source code
-                        chip.SetSourceCode(onDiskSourceCode);
-                        chip.SendUpdate();
-                        codeHashes[key] = onDiskHash; // Update the hash in the dictionary
-                        return true; // Update only one chip per iteration to avoid networking rejection
-                    }
-                  
                 }
+
+                // Always update chips in computers and laptops
+                var motherboards = FindObjectsOfType<ProgrammableChipMotherboard>();
+                foreach (var motherboard in motherboards)
+                {
+                    var computer = motherboard.ParentComputer;
+                    if (computer == null)
+                    {
+                        continue;
+                    }
+                    var filePrefix = $"{computer.DisplayName ?? "UNNAMED"} {motherboard.DisplayName ?? "UNNAMED"} [{motherboard.ReferenceId}]";
+                    if (UpdateChipDataIfNecessary(filePrefix, motherboard, directory))
+                    {
+                        return true; // If any chip was updated, return true to use shorter delay
+                    }
+                }
+               
             }
             catch (System.Exception ex)
             {
                 UnityEngine.Debug.LogError("Failed to write chip data: " + ex.Message);
             }
             return false;
+        }
+
+        public bool UpdateChipDataIfNecessary(string filePrefix, ISourceCode sourceCode, string directory)
+        {
+            var key = filePrefix;
+            string fileName = $"{key}.ic10";
+            string filePath = System.IO.Path.Combine(directory, fileName);
+
+            var sourceCodeText = sourceCode.GetSourceCode().ToString();
+            var hashBefore = sourceCodeText.GetHashCode();
+
+
+            if (!System.IO.File.Exists(filePath) || !codeHashes.ContainsKey(key) || codeHashes[key] != hashBefore) // file doesn't exist or code has changed
+            {
+                UnityEngine.Debug.LogWarning($"{key} source code exported. Before: {codeHashes.GetValueOrDefault(key)}, After: {hashBefore}");
+                ConsoleWindow.Print($"{key} source code exported. Before: {codeHashes.GetValueOrDefault(key)}, After: {hashBefore}", ConsoleColor.Blue);
+                System.IO.File.WriteAllText(filePath, sourceCodeText);
+            }
+            codeHashes[key] = hashBefore; // store current hash
+
+            // Check if the file exists and read its content
+            var onDiskSourceCode = System.IO.File.Exists(filePath) ? System.IO.File.ReadAllText(filePath) : string.Empty;
+            var onDiskHash = onDiskSourceCode.GetHashCode();
+
+            if (onDiskHash != hashBefore)
+            {
+                UnityEngine.Debug.LogWarning($"{key} source code loaded. Before: {hashBefore}, After: {onDiskHash}");
+                ConsoleWindow.Print($"{key} source code loaded. Before: {hashBefore}, After: {onDiskHash}", ConsoleColor.Green);
+                // If the hash doesn't match, write the new source code
+                sourceCode.SetSourceCode(onDiskSourceCode);
+                sourceCode.SendUpdate();
+                codeHashes[key] = onDiskHash; // Update the hash in the dictionary
+                return true; // Update only one chip per iteration to avoid networking rejection
+            }
+
+            return false; // No source code was updated
         }
     }
 
